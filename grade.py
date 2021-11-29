682d699479cb884012ab3a26a3a09e6fd24a8c20
Tamilarasan <87750255+tamil06448@users.noreply.github.com>
2021-11-29 11:30:26 +0530

    python program

---
+++ grade.py
@@ -0,0 +1,10 @@
+def gradings(grade):
+        if("grade>90 and grade<=100")
+            print("your grade is a")
+        elif("grade>80 and grade<=90")
+            print("your grade is b")
+        elif("grade>70 and grade<=80")
+            print("your grade is c")
+        else:
+            print("your failed")
+
