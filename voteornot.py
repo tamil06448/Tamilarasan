359cfa69492239c0523c26b022853376a0552c66
Tamilarasan <87750255+tamil06448@users.noreply.github.com>
2021-11-30 02:08:14 +0530

    Delete voteornot.py

--- voteornot.py
+++
@@ -1,8 +0,0 @@
-def voteornot(age):
-    if (age>18 and age<100):
-        print("yes you can vote")
-    else:
-        print("no you can not vote")
-
-age=int(input("enter your age: "))
-voteornot(age)
