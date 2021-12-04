1c77b92584200c13d41c0430315a92350a1118e2
Tamilarasan <87750255+tamil06448@users.noreply.github.com>
2021-12-04 22:02:43 +0530

    Delete TestIfElse.py

--- TestIfElse.py
+++
@@ -1,9 +0,0 @@
-def voterOrNot(age):
-    if(age>18 and age<100):
-        print("yes you can vote")
-    else:
-        print("no you can not vote")
-
-
-age  = int(input("Enter your age: "))
-voterOrNot(age)
